8689c3b23fcdc81ee03b1508e3ce1cba848e933f
Matthiew Purple <matthiewpurple@hotmail.com>
2023-05-18 17:48:28 +0200

    added self-destruct

--- TruePierce06.cs
+++ TruePierce06.cs
@@ -10,6 +10,7 @@ namespace true_pierce_06;
 public class TruePierce06 : MelonMod
 {
     private static bool hasPierce; // Remembers if the attacker has Pierce or Son's Oath/Raidou the Eternal
+    private static List<int> magicList = new List<int>() { 0, 1, 2, 3, 4, 5, 11 }; // Phys, Fire, Ice, Elec, Force, Almighty, Self-destruct
 
     // Before getting the effectiveness of a skill
     [HarmonyPatch(typeof(nbCalc), nameof(nbCalc.nbGetKoukaType))]
@@ -33,7 +34,7 @@ public class TruePierce06 : MelonMod
         public static void Postfix(ref uint __result, ref int attr, ref int formindex, ref int nskill)
         {
             // If the attack is not a repel, the attacker has Pierce, the attack is physical/magical/almighty and it's resisted/blocked/drained
-            if (nskill != -1 && hasPierce && attr >= 0 && attr <= 5 && (__result < 100 || (__result >= 65536 && __result < 131072) || (__result > 262143 && __result < 2147483648)))
+            if (nskill != -1 && hasPierce && magicList.Contains(attr) && (__result < 100 || (__result >= 65536 && __result < 131072) || (__result > 262143 && __result < 2147483648)))
             {
                 __result = 100; // Forces the affinity to become "neutral"
                 nbMainProcess.nbGetMainProcessData().d31_kantuu = 1; // Displays the "Pierced!" message
